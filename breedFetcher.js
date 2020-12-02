064422beca28416a25149e9a160b22f038c224a0
saoussen <sawsan_sli3i@live.fr>
2020-12-02 16:33:06 +0000

    return the body for siberian breed using request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
 const request = require ('request');
+const url = 'https://api.thecatapi.com/v1/breeds/search?name=';
+const breed = 'Siberian';
+const breedFetcher = function (url, breed) {
+  request(url+breed,(error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(typeof body);
+    const breedCat = JSON.parse(body);
+    console.log(breedCat);
+  });
+}
+breedFetcher (url, breed);
