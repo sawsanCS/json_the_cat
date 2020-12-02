0010bd21a70de2d2d0e82d7e649312ac5507fabd
saoussen <sawsan_sli3i@live.fr>
2020-12-02 17:08:32 +0000

    Handle request errors and print the error details to the screen

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,16 +3,18 @@ const myArgs = process.argv.slice(2);
 
 const url = 'https://api.thecatapi.com/v1/breeds/search?name=';
 const breed = myArgs[0];;
-const breedFetcher = function (url, breed) {
+const breedFetcher = function (breed, callback) {
   request(url+breed,(error, response, body) => {
     if (error) {
-      console.log(error);
+      callback(error, null);
       return;
     }
-    console.log(typeof body);
     const breedCat = JSON.parse(body);
-    console.log(typeof breedCat);
-    console.log(breedCat[0].description);
+   console.log(breedCat[0].name);
+   console.log(breedCat[0].description);
+    if (breed === breedCat[0].name) {
+      callback(null, breedCat[0].description);
+    } else { callback (null, 'breed not found');}
   });
-}
-breedFetcher (url, breed);
+};
+breedFetcher (breed,(x) => console.log(x));
